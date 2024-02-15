24102ba3536bac3f5e9f8e4b58a6ee8317620db0
Caio M. S. <caiocpe@gmail.com>
2024-02-15 19:18:26 -0300

    ⠀

---
+++ src/components/Accordion/index.ts
@@ -0,0 +1,5 @@
+import AccordionBody from "./AccordionBody";
+import AccordionHead from "./AccordionHead";
+import AccordionRoot from "./AccordionRoot";
+
+export {AccordionRoot, AccordionHead, AccordionBody}
